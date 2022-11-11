b7b1a43d7d3d7e6e86bfbcaeb04460638b1fd425
GeovaniLima <geovani.lima.silva@hotmail.com>
2022-11-11 11:29:15 -0300

    Tela Historico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,63 @@
+import { useState } from 'react';
 import { 
-  Center,
+  VStack,
+  Heading,
+  SectionList,
   Text
 } from 'native-base';
 
+import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
+
 export function History() {
+
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal']
+    }
+  ]);
+
   return(
-    <Center
+    <VStack
       flex={1}
     >
-      <Text color="white">History</Text>
-    </Center>
+      <ScreenHeader 
+        title="Historico de Exercicios"
+      />
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mt={10}
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={[].length === 0 && { flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Text
+            color="gray.100"
+            textAlign="center"
+          >
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos treindar hoje?
+          </Text>
+        )}
+      />
+    </VStack>
   )
 }
